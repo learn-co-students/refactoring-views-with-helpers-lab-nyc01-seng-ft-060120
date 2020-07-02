6393bdbdcffe3b25371990536186974d67a7eb08
mayalexandera <maya.blevins@gmail.com>
2020-07-02 13:42:03 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name if artist
   end
 
   def artist_name=(name)
+    assign_artist = Artist.find_or_create_by(name: name)
+    self.artist = assign_artist
   end
 end
